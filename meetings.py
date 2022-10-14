444a28f2b8e46ede4d9ff0c65cda1cc05f1a1a45
Chris Hartley <chris@hartley.id.au>
2022-10-14 11:32:38 +1000

    Completed meetings.py

---
+++ meetings.py
@@ -0,0 +1,35 @@
+import requests
+import pprint
+import os
+
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+headers = {"Content-Type": "application/json", "Authorization": f"Bearer {ACCESS_TOKEN}"}
+
+meetings_url = "https://webexapis.com/v1/meetings"
+meeting_start = "2022-10-18T11:00:00+11:00"
+meeting_end = "2022-10-18T11:30:00+11:00"
+invitees = [
+    {
+        "email": "user1@chhartle.wbx.ai",
+        "coHost": True
+    },
+    {
+        "email": "user2@chhartle.wbx.ai"
+    }
+]
+
+meeting_payload = {
+    "title": "Cisco Live Test Meeting",
+    "start": meeting_start,
+    "end": meeting_end,
+    "invitees": invitees,
+    "sendEmail": False
+}
+
+create_meeting = requests.post(meetings_url, headers=headers, json=meeting_payload)
+if create_meeting.status_code == 200:
+
+    pprint.pprint(create_meeting.json())
+else:
+    print(create_meeting.text)
